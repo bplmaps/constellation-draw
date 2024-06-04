0e6e82f188cd79fbb428c30642f78c9dc131e868
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2024-06-04 12:53:39 -0400

    fix typo

--- src/lib/Instructions.svelte
+++ src/lib/Instructions.svelte
@@ -7,7 +7,7 @@
     <p>
       At the <a href="https://leventhalmap.org" target="_blank">Leventhal Map & Education Center</a>, we strive to make the world a brighter place for all. And as we
       reach the end of our fiscal year, we are counting on our supporters to help
-      make our future work shine. Inspired by the <a href="https://www.leventhalmap.org/digital-exhibitions/heaven-and-earth/catalogue/4/" target="_blank">celestial chart in <i>Heaven & Earth</i></a>, we're creating a constellation chart drawn by our supporters
+      make our future work shine. Inspired by the <a href="https://www.leventhalmap.org/digital-exhibitions/heaven-and-earth/catalogue/4/" target="_blank">celestial chart in <i>Heaven & Earth</i></a>, we're creating a constellation chart drawn by our supporters. 
       <strong class="text-blue-200">Draw your very own constellation and then donate $20 or more to become part of our special community-drawn star chart.</strong>
     </p>
     <p class="text-sm"><a href="http://leventhalmap.org/donate" target="_blank">Learn more about donating to the Leventhal Center</a></p>
